c2146563f2a4d453457c2a23f1a4d32088b380aa
samirrajpurkar <samir.rajpurkar@gmail.com>
2018-10-26 20:57:00 +0400

    still nothing works

--- application.py
+++ application.py
@@ -2,18 +2,31 @@ import os
 import requests
 
 from flask import Flask, render_template, request, jsonify, redirect
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
+
+# Check for db and other environment variables
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+# check socketio environment
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-
-messages = {"name": 'Default Name', "post": 'Default post'}
-
-def get_messages():
-  return messages
 
 @app.route("/")
 def index():
@@ -22,48 +35,44 @@ def index():
 @app.route('/name', methods=['POST'])
 def handle_name():
   # save form data to the database
+  """Add name"""
   name = request.form.get("name")
-  name = str(name)
-  messages["name"] = name
-  return render_template("post.html")
+  return render_template("post.html", name=name)
 
 @app.route('/post', methods=["POST"])
 def handle_post():
   # save form data to the database
+  # Add Post
+
+  name = request.form.get("name")
   post = request.form.get("post")
-  post = str(post)
-  messages["post"] = post
-  #emit('newpost event', messages, broadcast=True)
-  return render_template("success.html")
+
+  try:
+    db.execute("INSERT INTO MESSAGES (name, post) VALUES (:name, :post)", {
+      "name": name,
+      "post": post
+      })
+    db.commit()
+  except ValueError:
+    return render_template("error.html", message="Error in inserting name")
+
+  try:
+    messages = db.execute("SELECT * FROM MESSAGES").fetchall()
+  except ValueError:
+    return render_template("error.html", message="Error in inserting name")
 
 
-# @socketio.on('post event')
-# def handle_post_event(message):
-#   print('-----handle_post_event--')
-#   print(message)
-#   emit('newpost event', message, broadcast=True)
+  return render_template("post.html", name = name, messages = messages)
 
+@socketio.on('new post')
+def handle_new_post(message):
+  print('-----name from handle_post_event--')
+  print(message)
 
-# @app.route("/submit_name", methods=["POST"])
-# def handle_submit_name():
-#   display_name = str(request.form.get("display_name")).replace(" ", "")
+  emit('newpost event',{
+    "name": message["name"],
+    "message": message["post"]
+    }, broadcast=True)  
   
-#   messages["display_name"] = display_name
-#   print('-----------submit_name----------')
-#   print(messages)
-#   return render_template("posts.html", messages=messages)
 
-# @app.route("/post_message", methods=["POST"])
-# def handle_post_message():
-#   display_name = str(request.form.get("display_name")).replace(" ", "")
-#   post = request.form.get("post_message")
-#   messages["display_name"] = display_name
-#   messages["post"] = post
-#   print('----post_message--------')
-#   print(messages)
-#   return render_template("posts.html", messages = messages)
   
-@socketio.on("submit message")
-def handle_message():
-  emit("new message",messages, broadcast=True)
-
--- static/flack.js
+++ static/flack.js
@@ -1,49 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   
+// Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-  var message = {
-    name: 'samir',
-    post: 'first post'
-  }
-
-  var flag = false
-
-if(document.querySelector('#index_name')) {
-    name = document.querySelector('#index_name').value
-    message.name = name
-    console.log(message)
-  }
-  
- if(document.querySelector('#post_post')) {
-    post = document.querySelector('#post_post').value
-    message.post = post
-    console.log(message)
-    flag = true
-  }
-
-  // Connect to websocket
-  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-  socket.on('connect',() => { 
-    console.log('Client connected');
-    if(document.querySelector('#button')) {
-      button = document.querySelector('#button')
-      button.onclick = ( ) => {
-        console.log('click')
-        console.log(message)
-        socket.emit('post event', message)
+    // When connected, configure buttons
+    socket.on('connect', () => {
+      if (document.querySelector('#form-post')) {
+        document.querySelector('#form-post').onsubmit = (event) => {
+          name = document.querySelector('#form-name').value
+          post = document.querySelector('#form-post').value
+          console.log({"name": name, "post": post})
+          socket.emit('new post', {"name": name, "post": post})
+        }
       }
-    }
-    // // Each button should emit a "submit vote" event
-    //   document.querySelector('#button').forEach(button => {
-    //       button.onclick = () => {
-    //           console.log('click')
-    //           socket.emit('post event', message);
-    //       };
-    //   });
-  }) 
+    });
 
-  socket.on('newpost event', post => {
-    console.log(post);
-  })
-
+    socket.on('newpost event', (message) => {
+      console.log(message);
+      if(document.querySelector('#yes')) {
+        document.querySelector('#yes').innerHTML = message.name + " : " + message.message;  
+      }
+    });
+      
 });
